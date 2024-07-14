4e1e3fb0dc76c1a95be13423a709f95c780b6daf
Mohansundaram <maniarumugam165@gmail.com>
2024-07-14 20:31:03 +0530

    constructor

---
+++ constructor.py
@@ -0,0 +1,10 @@
+class laptop():
+    def __init__(self,par1,par2):
+        self.par1=par1
+        self.par2=par2
+    def ram(self):
+        print(self.par1)
+        print(self.par2)
+obj=laptop("part1","part2")
+obj.ram()
+    
