74a52213fe141c4e92f1c1838208983a2ca1fc1d
Mohansundaram <maniarumugam165@gmail.com>
2024-07-20 23:46:46 +0530

    example

--- swap variable without use third variable.py
+++ swap variable without use third variable.py
@@ -6,11 +6,19 @@ a=b
 b=temp
 print(a)
 print(b)
+print("second method")
 #swap the value without use the third variable:  
 a=10
 b=20
 b=a+b
 a=b-a
 b=b-a
+print(a)
+print(b)
+print("third method")
+#next way
+a=10
+b=20
+a,b=b,a
 print(a)
 print(b)
