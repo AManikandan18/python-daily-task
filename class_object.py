7341b025ec683fd7ebbf40c0e50979910d9d40a8
Mohansundaram <maniarumugam165@gmail.com>
2024-11-26 11:25:27 +0530

    class and object

---
+++ class_object.py
@@ -0,0 +1,16 @@
+class laptops:
+    price="10000"
+    processor="i?"
+    ram="gb"
+    def hp(self):
+        print("hp laptop")
+    def lenovo(self):
+        print("lenovo laptop")
+    def dell(self):
+        print("dell laptop")
+
+hp=laptops()
+print(hp.price)
+lenovo=laptops()
+
+dell=laptops()
