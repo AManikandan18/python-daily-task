ee65672fe3be6696955872d32dbcd160c9f54a01
Mohansundaram <maniarumugam165@gmail.com>
2024-11-26 18:49:49 +0530

    greatest number amoung 3 numbers

---
+++ greatest number.py
@@ -0,0 +1,10 @@
+a=int(input("Enter you A value: "))
+b=int(input("Enter you A value: "))
+c=int(input("Enter you A value: "))
+if a>c:
+    if a>b:
+        print("The value A",a,"is greater than b and c")
+    else:
+        print("The value B",b,"is greater than a and c")
+else:
+    print("The value C",c,"is greater than a and b")
