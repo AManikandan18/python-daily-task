e40c92df8f686c26ea0f18b4cddf3d5d10459dee
Mohansundaram <maniarumugam165@gmail.com>
2024-11-26 19:04:46 +0530

    find greatest value

--- greatest number.py
+++ greatest number.py
@@ -1,10 +1,20 @@
 a=int(input("Enter you A value: "))
-b=int(input("Enter you A value: "))
-c=int(input("Enter you A value: "))
+b=int(input("Enter you B value: "))
+c=int(input("Enter you C value: "))
 if a>c:
     if a>b:
         print("The value A",a,"is greater than b and c")
     else:
         print("The value B",b,"is greater than a and c")
+else:
+    print("The value C",c,"is greater than a and b")
+
+
+##another way:
+
+if a>b and a>c:
+    print("The value A",a,"is greater than b and c")
+elif b>a and b>c:
+    print("The value B",b,"is greater than a and c")
 else:
     print("The value C",c,"is greater than a and b")
