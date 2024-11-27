d4371e8cf0e264ed796bf958b24f7fe2df64d23a
Mohansundaram <maniarumugam165@gmail.com>
2024-11-27 17:51:21 +0530

    comrehention in all types

---
+++ comprehention.py
@@ -0,0 +1,10 @@
+a=1
+if a%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+##list comprehention:
+list_2=[print("even number") if(a%2==0) else print("odd number")]
+
+set_2={print("even number") if(a%2==0) else print("odd number")}
