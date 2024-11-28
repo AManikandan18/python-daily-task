ec8e6030e9bf334b8920c73b4a7c0e477f34244c
Mohansundaram <maniarumugam165@gmail.com>
2024-11-28 12:46:18 +0530

    list comprehension using forloop

--- break_continue.py
+++ break_continue.py
@@ -8,19 +8,5 @@ while i<10:
         break
     print(i)
 
-x=10
-if x>5: print("true")
 
-print("true")if x>0 else print("wrong")
-
-print("greater") if (x>55) else print("lessthan") if x==10 else print("lower")
-
-a=1
-if a%2==0:
-    print("even number")
-else:
-    print("odd number")
-
-list_2=[print("even number") if(a%2==0) else print("odd number")]    
-    
 
--- comprehention.py
+++ comprehention.py
@@ -4,7 +4,21 @@ if a%2==0:
 else:
     print("odd number")
 
+
+##if condition:
+    
 ##list comprehention:
 list_2=[print("even number") if(a%2==0) else print("odd number")]
 
+##set:
 set_2={print("even number") if(a%2==0) else print("odd number")}
+
+##for loop:
+
+##list
+list2=[i for i in range(1,10) if(i%2==0)]
+print("even number is :",list2)
+
+##set
+set2=[x for x in range(1,10) if(x%2==0)]
+print("even number is: ",set2)
