bd797cbb8e405a734f87faf444a1cfc3728bc4b3
Mohansundaram <maniarumugam165@gmail.com>
2024-11-28 17:05:36 +0530

    factorial using while loop

--- factorial_number.py
+++ factorial_number.py
@@ -1,9 +1,20 @@
 ##Factorial by using For loop:
-a=int(input("Enter your value: "))
+##a=int(input("Enter your value: "))
+##if a==0 or a<0:
+##    print("\"wrong\" enter the valid input: ")
+##else:
+##    fact=1
+##    for i in range(1,a+1):
+##        fact*=i
+##    print("The Factorial number is: ",fact)    
+
+##While loop:
+a=int(input("Enter the value: "))
 if a==0 or a<0:
     print("\"wrong\" enter the valid input: ")
 else:
     fact=1
-    for i in range(1,a+1):
-        fact*=i
+    while(a>0):
+        fact*=a
+        a-=1
     print("The Factorial number is: ",fact)    
