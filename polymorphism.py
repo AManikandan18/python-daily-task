819519b2ea76009f02bb10b6d9afffcb46d5cbda
Mohansundaram <maniarumugam165@gmail.com>
2024-12-02 14:58:06 +0530

    polymorphism content

---
+++ polymorphism.py
@@ -0,0 +1,8 @@
+## polymorphism :
+
+##* polymorphism is overriding in python
+##* polymorphism is used to enakku ore function la 2 argument ha pass pannalum
+##  enakku output varanum and adhe function la 3 argument pass pannalum enakku output
+##  varanum that is called (same name but different signature).
+##  
+
