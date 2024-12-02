cb59d1465ae3a74f7cc1e581ddda7dd5a9af3564
Mohansundaram <maniarumugam165@gmail.com>
2024-12-02 16:16:43 +0530

    polymorphism overriding

--- polymorphism.py
+++ polymorphism.py
@@ -1,8 +1,39 @@
 ## polymorphism :
 
-##* polymorphism is overriding in python
+##* polymorphism is overriding in python.
+##* polymorphism means many ways to solve one problem.
 ##* polymorphism is used to enakku ore function la 2 argument ha pass pannalum
-##  enakku output varanum and adhe function la 3 argument pass pannalum enakku output
-##  varanum that is called (same name but different signature).
+##  andha numbers add(+) agi enakku output varanum and adhe function la 3
+##  argument pass pannalum enakku output varanum that is called (same name
+##  but different signature).
 ##  
 
+## 1 Way:
+##This way is called same function but different singnature:
+
+def display(a,b,c=5):
+    print(a+b+c)
+display(5,5) #na first rendu(2) value va argument ha pass pandrappavum enakku
+             #program run agi output varanum and...
+
+display(10,10,10) #moonu argument ha pass pannum pothum enakku three values hum
+                  #add agi output varanum.
+
+## Second Way:
+
+class animal():
+    def sound(self):
+        print("animal make sound")
+
+class pets():
+    def sound(self):
+        print("dog make noice")
+    def satham(self):
+        print("ore satham")
+
+class bird(pets,animal):
+    def sound(self):
+        print("bird sing")  
+
+b1=bird()
+b1.satham()
