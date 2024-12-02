a917423b8be83d7fba3e11e324ac5e39f8ab7f57
Mohansundaram <maniarumugam165@gmail.com>
2024-12-02 13:01:24 +0530

    super function explaination

---
+++ super_keyword.py
@@ -0,0 +1,12 @@
+"""super keyword is used to inherit the parent class and mainly used to namma oru
+3 class ha create pannitu mela ulla 2 class kku init use pannitu then andha rendu(2)
+class ha yum moonavathu(third) class la inherit pannumpothu third class la
+__init__() method use pannirundha andha oru __init__() method tha print agum so
+we use the super().__init__() method inherit panna class la ha __init__()
+irundha adhayum sethu print pannum that is reason to we use the super().__init__()
+function"""
+
+
+
+
+
