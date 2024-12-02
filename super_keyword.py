a4d80af065cf7859fed2c3bff3874c5835d8a6f2
Mohansundaram <maniarumugam165@gmail.com>
2024-12-02 13:02:11 +0530

    super function program

--- super_keyword.py
+++ super_keyword.py
@@ -7,6 +7,43 @@ irundha adhayum sethu print pannum that is reason to we use the super().__init__
 function"""
 
 
+class a():
+    def __init__(self):
+        print("class A")
+    def display(self):
+        print("calss a is display")
 
+class b():
+    def __init__(self):
+        super().__init__()
+        print("class b")
+    def display(self):
+        print("calss b is display")
+
+class c():
+    def __init__(self):
+        print("class c")
+        super().__init__()
+    def display(self):
+        print("class c is display")
+
+class d():
+    def __init__(self):
+        super().__init__()
+        print("class d")
+    def display(self):
+        print("calss d is display")
+
+class e(a,b,c,d):
+    def __init__(self):
+        super().__init__()
+        print("class e")
+    def display():
+        print("calss e is display")
+
+
+obj2=e()
+## mro used to see the (orderwise) or see chaining class:  
+print(e.__mro__)
 
 
