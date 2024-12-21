f0f25da73bc0da5da757c7037789f01af1ff1acd
Mohansundaram <maniarumugam165@gmail.com>
2024-12-21 19:07:26 +0530

    example linked list and hash map

--- DSA/hashmap.py
+++ DSA/hashmap.py
@@ -1,10 +1,14 @@
 # ------ hash method ----- how its runn....
-print(hash("mani"))
+
+# print(hash("mani"))
+
 #this hash map give a one value  that is (4644169627606722112)
 # and run again print("mani") statement that is return the different value (81842174250153080)
 # every time you run and get different value but,
-print(hash("mani"))
-print(hash("mani")) #run two print statement is same time return the value is same 
+
+# print(hash("mani"))
+# print(hash("mani")) #run two print statement is same time return the value is same 
+
 # 7727166489213030476
 # 7727166489213030476 because this two print stement is run in same "session" same session la 
 # enakku two print statement run aguthu
@@ -15,4 +19,36 @@ class HashMap:
     
     def __init__(self,size):
         self.size=size
-        self.hashIndex=
+        self.hashIndex=[None]*self.size
+
+    def get_index(self,key):
+        return hash(key)%self.size
+    
+    def __setitem__(self,key,value):
+        index=self.get_index(key)
+        print(index)
+        if self.hashIndex[index] is None:
+            self.hashIndex[index]=[[key,value]]
+            print(self.hashIndex)
+        else:
+            self.hashIndex[index].append([key,value])
+            print(self.hashIndex)
+
+    def Get(self,key):
+        index=self.get_index(key)
+        if self.hashIndex[index]:
+            for val in self.hashIndex[index]:
+                if val[0]==key:
+                    print(val[1])
+        else:
+            print("your value" ,key,"is not here")
+        
+    
+obj=HashMap(10)
+# obj.hashIndex
+# obj.add("name","mani")
+# obj.add("age",18)
+obj["roll"]="2026J09" #this code for when you using the bild-in-function def __setitem__():
+obj.Get("roll")
+# obj["roll"]="2026J09"
+# obj.Get("roll")
--- DSA/linked_list_DSA.py
+++ DSA/linked_list_DSA.py
@@ -30,60 +30,7 @@
 #     cur=cur.pointer
 
 
-
-
-
-# class Node:
-#     def __init__(self,data):
-        # self.data=data
-#         self.pointer=None
-    
-
-#     def add(self,value):
-#         self.pointer=value
-#         print(self.pointer)
-#         # print(self.pointer)
-#         # return self.pointer
-    
-#     def display(self):
-#         # if self.pointer is not None:
-#         # if self.pointer:
-#         while(self.pointer is not None):
-#             print(self.data)
-#             self.data=self.pointer  
-#             break
-    # def display(self):
-    #     # if self.pointer is not None:
-    #     if self.pointer:
-    #         print("empty")
-    #     else:
-    #         while(self.data is not None):
-    #             print(self.data)
-    #             self.data=self.pointer  
-
-    # def insert(self,index,value):
-    #     self.linked_list.insert(index,value)
-    #     print(self.linked_list)
-    # def delete(self,value):
-    #     rmv=self.linked_list.remove(value)
-    #     print(rmv)
-    # def isempty(self):
-    #     if self.linked_list:
-    #         return False
-    #     else:
-    #         return True    
-
-# head=Node(5)
-# head2=Node(6)
-# head3=Node(7)
-# # head.add(7)
-# # head.add(8)
-# print("the ",head2)
-# head.add(head2)
-# head.add(head3)
-# # print(head3)
-# head.display()
-
+# -------------------
 
 class Node:
     def __init__(self,data):
@@ -106,7 +53,17 @@ class LinkedList:
         cur = self.head 
         while(cur is not None):
             print(cur.data,end=" ")                                     
-            cur=cur.pointer       
+            cur=cur.pointer      
+
+    def remove(self,data) :
+        if self.head is not None:
+            if self.head.data==data:
+                self.head=self.head.pointer
+            else:
+                cur=self.head
+                while(cur.pointer is not None):
+
+
 
 linked=LinkedList()
 linked.add(5)                
