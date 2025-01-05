f9bfb63eecc2dbce1703d1bceca8773a1cff353c
AManikandan18 <maniarumugam165@gmail.com>
2025-01-06 00:02:49 +0530

    vowels program

--- vowels program.py
+++ vowels program.py
@@ -1,23 +1,23 @@
-##count vowels in string:
-a=input("enter your string")
-count=0
-for i in a:
-    if i  in "aeiouAEIOU":
-       count+=1
-print(count)
+####count vowels in string:
+##a=input("enter your string")
+##count=0
+##for i in a:
+##    if i  in "aeiouAEIOU":
+##       count+=1
+##print(count)
+##
 
-
-## print vowels in list:
+## print vowels in list program:
 a=int(input("enter the count for how many time do you want enter the string: "))
 values=[]
 vowels_value=[]
+Vowels="aeiouAEIOU"
 for i in range(a):
     value=input("enter the string value: ")
     values.append(value)
-##print(values)
 for i in values:
     for j in i:
-        if j in "aeiouAEIOU":
+        if str(j) in Vowels:
             vowels_value.append(i)
 
 print("vowel words",list(set(vowels_value))) 
