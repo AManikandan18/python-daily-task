8759aa4c964f8732a45c317309ba4eb805ea2d32
AManikandan18 <maniarumugam165@gmail.com>
2025-01-06 17:34:09 +0530

    Palindrome prgram

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,15 @@
 ##palindrome:
+#first way:
+a="mani"
+rev_value=""
+for i in a:
+    rev_value=i+rev_value
+if a==rev_value:
+    print("This is Palindrome")
+else:
+    print("Not a Palindrome")
+
+##second way:
 a=input("enter youe value: ")
 if a==a[::-1]:
     print("YES,your word",a,"is palindrome.")
